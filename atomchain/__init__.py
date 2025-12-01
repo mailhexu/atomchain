f21180261b38bb435be79a8b7f3a998caf31c623
mailhexu <mailhexu@gmail.com>
2025-12-01 12:54:20 +0100

    update pyproject.toml

--- atomchain/__init__.py
+++ atomchain/__init__.py
@@ -1,3 +1,9 @@
 """
 Machine learning potentials
 """
+
+from atomchain.init_model import init_calc
+from atomchain.phonon.mlphonon import phonon_with_ml
+from atomchain.relax import relax_with_ml
+
+__all__ = [init_calc, phonon_with_ml, relax_with_ml]

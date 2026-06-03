3b800b7cec629056566b9984378e8053ffb6e62e
mailhexu <mailhexu@gmail.com>
2026-06-03 17:26:45 +0200

    update package exports and CLI metadata

--- atomchain/__init__.py
+++ atomchain/__init__.py
@@ -9,6 +9,15 @@ from atomchain.ddb import write_ddb_from_finite_difference, write_ddb_from_phono
 from atomchain.gap import predict_gap
 from atomchain.init_model import init_calc
 from atomchain.io import read_abinit_hist, write_abinit_hist
+from atomchain.materials_project import (
+    MaterialsProjectStructure,
+    get_structure_by_id,
+    get_structure_record_by_id,
+    get_structures_by_spacegroup,
+    query_structures,
+    write_manifest,
+    write_structure_records,
+)
 from atomchain.md import (
     md_npt,
     md_npt_berendsen,
@@ -19,6 +28,7 @@ from atomchain.md import (
     md_nvt_langevin,
 )
 from atomchain.metastable import explore_metastable_states
+from atomchain.multibinit_workflow import run_model_build_workflow
 from atomchain.neb import calculate_neb
 from atomchain.phonon.mlphonon import phonon_with_ml
 from atomchain.rattle import generate_rattle_dataset
@@ -28,6 +38,19 @@ from atomchain.supercell import make_supercell_structure
 from atomchain.training import (
     generate_multibinit_training_artifacts,
     generate_training_trajectory,
+)
+from atomchain.training_set_generation import (
+    ContourSamplerConfig,
+    GeneratedFrame,
+    MetastableInterpolationConfig,
+    NptMdSamplerConfig,
+    RattleSamplerConfig,
+    SamplingContext,
+    generate_contour_frames,
+    generate_frames_from_source,
+    generate_metastable_interpolation_frames,
+    generate_npt_md_frames,
+    generate_rattle_frames,
 )
 
 __all__ = [
@@ -43,12 +66,31 @@ __all__ = [
     "calculate_neb",
     "collect_structures",
     "explore_metastable_states",
+    "run_model_build_workflow",
+    "MaterialsProjectStructure",
+    "get_structure_by_id",
+    "get_structure_record_by_id",
+    "get_structures_by_spacegroup",
+    "query_structures",
+    "write_structure_records",
+    "write_manifest",
     "write_ddb_from_finite_difference",
     "write_ddb_from_phonopy",
     "read_abinit_hist",
     "write_abinit_hist",
     "generate_training_trajectory",
     "generate_multibinit_training_artifacts",
+    "GeneratedFrame",
+    "SamplingContext",
+    "RattleSamplerConfig",
+    "MetastableInterpolationConfig",
+    "ContourSamplerConfig",
+    "NptMdSamplerConfig",
+    "generate_rattle_frames",
+    "generate_metastable_interpolation_frames",
+    "generate_contour_frames",
+    "generate_npt_md_frames",
+    "generate_frames_from_source",
     "md_nve_velocity_verlet",
     "md_nvt_langevin",
     "md_nvt_berendsen",
